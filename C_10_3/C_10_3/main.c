17fc1f0082570c0657cd77e0f85f8ec9626315aa
BUGmakerrr <799745201@qq.com>
2019-10-21 15:19:22 +0800

    commit

---
+++ C_10_3/C_10_3/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+	int i, j = 0;
+	int q[3][2] = {
+		{ 1, 2 },
+		{ 3, 4 },
+		{ 5, 6 },
+	};
+	printf("%d", q[1][0]);
+	system("pause");
+	return 0;
+}
